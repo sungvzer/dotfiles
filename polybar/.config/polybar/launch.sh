a1623777739f3efee2ad9858a202b54e06212c54
sungvzer <sungvzer@proton.me>
2023-06-07 15:30:34 +0200

    feat: additional polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+# Launch bars
+echo "---" | tee -a /tmp/polybar.log
+polybar default 2>&1 | tee -a /tmp/polybar.log & disown
