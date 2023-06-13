1fc2256a857d544b7f15fbbed99fddc1f6032f98
sungvzer <sungvzer@proton.me>
2023-06-13 19:46:29 +0200

    feat(neovim): add Markdown preview plugin

---
+++ neovim/.config/nvim/after/plugin/markdown-preview.lua
@@ -0,0 +1,5 @@
+-- Start the preview after entering a markdown file
+vim.g.mkdp_auto_start=true
+
+-- Stop the preview when exiting a markdown file
+vim.g.mkdp_auto_close = true
--- neovim/.config/nvim/lua/salviog/packer.lua
+++ neovim/.config/nvim/lua/salviog/packer.lua
@@ -79,4 +79,6 @@ return require('packer').startup(function(use)
     end
 
     use 'mogelbrod/vim-jsonpath'
+
+    use { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }
 end)
