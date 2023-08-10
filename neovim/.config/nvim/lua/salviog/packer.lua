606cbb427a3e9fb1412f5694ed70da2bf815e7b9
sungvzer <sungvzer@proton.me>
2023-08-11 00:55:15 +0200

    feat: add todo-comments

--- neovim/.config/nvim/lua/salviog/packer.lua
+++ neovim/.config/nvim/lua/salviog/packer.lua
@@ -1,8 +1,12 @@
 local ensure_packer = function()
     local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    local install_path = fn.stdpath('data') ..
+                             '/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        fn.system({
+            'git', 'clone', '--depth', '1',
+            'https://github.com/wbthomason/packer.nvim', install_path
+        })
         vim.cmd [[packadd packer.nvim]]
         return true
     end
@@ -15,24 +19,26 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        requires = {{'nvim-lua/plenary.nvim'}}
     }
 
     -- COLOR SCHEMES
-    use { "catppuccin/nvim", as = "catppuccin" }
-    use { "frenzyexists/aquarium-vim", as = "aquarium" }
-    use { 'Everblush/nvim', as = 'everblush' }
+    use {"catppuccin/nvim", as = "catppuccin"}
+    use {"frenzyexists/aquarium-vim", as = "aquarium"}
+    use {'Everblush/nvim', as = 'everblush'}
     use 'sainnhe/everforest'
-
 
     use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            local ts_update = require('nvim-treesitter.install').update({
+                with_sync = true
+            })
             ts_update()
-        end,
+        end
     }
 
     use 'mbbill/undotree'
@@ -42,30 +48,24 @@ return require('packer').startup(function(use)
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
+            {'neovim/nvim-lspconfig'}, -- Required
             {
                 -- Optional
                 'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
+                run = function() pcall(vim.cmd, 'MasonUpdate') end
+            }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
+            {'hrsh7th/nvim-cmp'}, -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'} -- Required
         }
     }
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
+            'nvim-tree/nvim-web-devicons' -- optional
         },
-        config = function()
-            require("nvim-tree").setup {}
-        end
+        config = function() require("nvim-tree").setup {} end
     }
 
     use 'sbdchd/neoformat'
@@ -74,18 +74,25 @@ return require('packer').startup(function(use)
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+    if packer_bootstrap then require('packer').sync() end
 
     use 'mogelbrod/vim-jsonpath'
 
-    use { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }
+    use {
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = {"markdown"} end,
+        ft = {"markdown"}
+    }
 
     use 'f-person/git-blame.nvim'
 
     use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        'nvim-lualine/lualine.nvim',
+        requires = {'nvim-tree/nvim-web-devicons', opt = true}
     }
+
+    use {'folke/trouble.nvim', requires = {'nvim-tree/nvim-web-devicons'}}
+    use "nvim-lua/plenary.nvim"
+    use {'folke/todo-comments.nvim', requires = {'nvim-lua/plenary.nvim'}}
 end)
