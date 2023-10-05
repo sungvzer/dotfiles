3e68e62f7418a30f173c0f33c48f5febcbae961b
sungvzer <sungvzer@proton.me>
2023-10-05 02:49:09 +0200

    fix: show hidden files in telescope

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,21 +1,24 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pt', builtin.lsp_dynamic_workspace_symbols, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", ':lua require"telescope.builtin".find_files({ hidden = true })<CR>', {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>pt", builtin.lsp_dynamic_workspace_symbols, {})
 local actions = require("telescope.actions")
 
 require("telescope").setup({
-    pickers = {
-        colorscheme = {
-            enable_preview = true
-        }
-    },
-    defaults = {
-        mappings = {
-            i = {
-                ["<esc>"] = actions.close,
-            },
-        },
-    },
+	pickers = {
+		colorscheme = {
+			enable_preview = true,
+		},
+	},
+	defaults = {
+		file_ignore_patterns = {
+			".*.git/",
+		},
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+	},
 })
