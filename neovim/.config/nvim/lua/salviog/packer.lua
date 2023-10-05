0cb2e1c536fa633483aaf6e88752428584fae85e
sungvzer <sungvzer@proton.me>
2023-10-05 02:57:18 +0200

    feat: add tmux to the tmix :')

--- neovim/.config/nvim/lua/salviog/packer.lua
+++ neovim/.config/nvim/lua/salviog/packer.lua
@@ -116,4 +116,20 @@ return require("packer").startup(function(use)
 	use({ "folke/trouble.nvim", requires = { "nvim-tree/nvim-web-devicons" } })
 	use("nvim-lua/plenary.nvim")
 	use({ "folke/todo-comments.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use({
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+	})
 end)
