3005bcf391ac22bd4f8b19f58ab31077d7349d38
sungvzer <sungvzer@proton.me>
2023-12-19 13:35:22 +0100

    feat: ignore node_modules in filesystem_watchers

--- neovim/.config/nvim/after/plugin/nvim-tree.lua
+++ neovim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,20 +1,27 @@
 require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    view = {
-        width = 50,
-        side = 'right',
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = false,
-        git_clean = false
-    },
-    git = {
-        ignore = false
-    },
-    update_focused_file = {
-        enable = true
-    },
+	sort_by = "case_sensitive",
+	view = {
+		width = 50,
+		side = "right",
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+		git_clean = false,
+	},
+	git = {
+		ignore = false,
+	},
+	update_focused_file = {
+		enable = true,
+	},
+	filesystem_watchers = {
+		enable = true,
+		debounce_delay = 50,
+		ignore_dirs = {
+			"node_modules",
+		},
+	},
 })
