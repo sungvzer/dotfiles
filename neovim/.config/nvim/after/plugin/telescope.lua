9feb2b165eb3a33e01814fc963851b66efadc8af
sungvzer <sungvzer@proton.me>
2024-04-05 15:43:37 +0200

    feat(nvim): show search in hidden filed with ripgrep

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -12,6 +12,18 @@ require("telescope").setup({
 		},
 	},
 	defaults = {
+		vimgrep_arguments = {
+			{
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--hidden",
+			},
+		},
 		file_ignore_patterns = {
 			".*.git/",
 		},
