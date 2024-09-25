9e39cf199737bdb4ca5c6410d1a0a94e6edadd49
sungvzer <sungvzer@proton.me>
2024-09-25 09:46:26 +0200

    feat(neovim): add surround

--- neovim/.config/nvim/lua/salviog/packer.lua
+++ neovim/.config/nvim/lua/salviog/packer.lua
@@ -154,4 +154,14 @@ return require("packer").startup(function(use)
 			"nvim-lua/plenary.nvim",
 		},
 	})
+
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
 end)
