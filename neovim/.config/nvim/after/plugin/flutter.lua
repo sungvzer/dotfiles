283610aefe2c4a60b11e76b9c33d4dc3bcacbac9
sungvzer <sungvzer@proton.me>
2024-10-14 11:08:09 +0200

    feat: remove unused flutter binding

--- neovim/.config/nvim/after/plugin/flutter.lua
+++
@@ -1,8 +0,0 @@
-require("flutter-tools").setup({})
-
-vim.keymap.set(
-	{ "n" },
-	"<leader>fl",
-	":vsplit | buffer __FLUTTER_DEV_LOG__<CR>",
-	{ remap = false, desc = "Open flutter dev log in vertical split" }
-)
