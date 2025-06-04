e89db75b8c71132d32e61e0e32017bdcd9083c62
sungvzer <sungvzer@proton.me>
2025-06-04 19:12:02 +0200

    feat: nvim bars overhaul

--- neovim/.config/nvim/after/plugin/lualine.lua
+++ neovim/.config/nvim/after/plugin/lualine.lua
@@ -3,7 +3,7 @@ require("lualine").setup({
 		icons_enabled = true,
 		theme = "auto",
 		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = { statusline = {}, winbar = {} },
 		ignore_focus = {},
 		always_divide_middle = true,
@@ -11,12 +11,12 @@ require("lualine").setup({
 		refresh = { statusline = 200, tabline = 1000, winbar = 50 },
 	},
 	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "diagnostics" },
+		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+		lualine_b = { "filename", "diagnostics" },
 		lualine_c = {},
-		lualine_x = { "branch", "diff", "filetype" },
-		lualine_y = { "encoding" },
-		lualine_z = { "location" },
+		lualine_x = { "navic" },
+		lualine_y = {},
+		lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
 	},
 	inactive_sections = {
 		lualine_a = {},
@@ -27,26 +27,7 @@ require("lualine").setup({
 		lualine_z = {},
 	},
 	tabline = {},
-	winbar = {
-		lualine_a = { "filename" },
-		lualine_b = {
-			"navic",
-
-			-- Component specific options
-			color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
-			-- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
-			-- Setting it to "static" will perform a adjustment once when the component is being setup. This should
-			--   be enough when the lualine section isn't changing colors based on the mode.
-			-- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
-			--   the current section.
-
-			navic_opts = nil, -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
-		},
-	},
-	inactive_winbar = {
-		lualine_a = {
-			"filename",
-		},
-	},
+	winbar = {},
+	inactive_winbar = {},
 	extensions = {},
 })
