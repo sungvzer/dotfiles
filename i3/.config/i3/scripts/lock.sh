f767db8ff91b4d5cd734b813e0b7c6293cdcf447
sungvzer <sungvzer@proton.me>
2025-07-05 18:35:22 +0200

    chore(i3): increase lock timer

--- i3/.config/i3/scripts/lock.sh
+++ i3/.config/i3/scripts/lock.sh
@@ -6,9 +6,9 @@ export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
-  --timer 60 \
+  --timer 270 \
     '~/.config/i3/scripts/brightness.sh push 10' \
     '~/.config/i3/scripts/brightness.sh pop' \
-  --timer 10 \
+  --timer 30 \
     '~/.config/i3/scripts/brightness.sh set 100; light-locker-command -l' \
     ''
