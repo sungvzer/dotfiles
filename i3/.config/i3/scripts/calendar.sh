b17a54a433729df1e6698ef43e83d240a9d22d21
sungvzer <sungvzer@proton.me>
2025-11-12 23:46:24 +0100

    feat(i3): do not kill betterbird, move to scratchpad instead

--- i3/.config/i3/scripts/calendar.sh
+++ i3/.config/i3/scripts/calendar.sh
@@ -1,6 +1,20 @@
-case $BLOCK_BUTTON in
-    1) i3-msg -q "exec --no-startup-id betterbird -calendar" ;;  # Left click, open calendar
-esac
+if [ $BLOCK_BUTTON = "1" ]; then
+    if pgrep -x "betterbird-bin" > /dev/null; then
+        # if Betterbird is in the scratchpad
+        if [ "$(~/.config/i3/scripts/get_window_by_mark.sh -m betterbird -r w)" = "-1" ]; then
+            i3-msg -q "[class=Betterbird] scratchpad show"
+            i3-msg -q "[class=Betterbird] focus"
+            i3-msg -q "[class=Betterbird] floating disable"
+    else
+            current_workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | tr -d '"')
+            i3-msg -q "[class=Betterbird] focus"
+            i3-msg -q "[class=Betterbird] move scratchpad"
+            i3-msg -q "workspace $current_workspace"
+        fi
+    else
+        i3-msg -q "exec --no-startup-id betterbird -calendar"
+    fi
+fi
 
 date '+%A %d/%m/%Y'
 
