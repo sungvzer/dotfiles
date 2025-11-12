206043ea26e3ffc87d3950bf3032e9c32d231941
sungvzer <sungvzer@proton.me>
2025-11-12 23:45:47 +0100

    feat(i3): set up auto mouse sensitivity

---
+++ i3/.config/i3/scripts/mouse_sensitivity.sh
@@ -0,0 +1,14 @@
+MOUSE_SENSITIVITY=${MOUSE_SENSITIVITY:-0.1}
+
+ while true; do
+  NEW_ID=$(xinput | grep 'G7 Mouse' | grep -o -E 'id=[0-9]+' | grep -o -E '[0-9]+')
+  if [ "$MOUSEID" != "$NEW_ID" ]; then
+    MOUSEID=$NEW_ID
+    if [ "$MOUSEID" != "" ]; then
+        xinput --set-prop $MOUSEID 147 "$MOUSE_SENSITIVITY" 0 0 0 "$MOUSE_SENSITIVITY" 0 0 0 1;
+        echo "Mouse sensitivity set for device ID $MOUSEID" >> ~/mouse_sensitivity.log
+    fi
+  fi
+  sleep 2
+done
+
