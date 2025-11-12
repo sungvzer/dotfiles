1fc98daf50dd12c0a8556e43a8bf46f8da59a8eb
sungvzer <sungvzer@proton.me>
2025-11-12 23:47:29 +0100

    feat: kill pavucontrol if right click again on volume

--- i3/.config/i3/scripts/volume.sh
+++ i3/.config/i3/scripts/volume.sh
@@ -12,9 +12,17 @@ get_volume() {
     amixer get Master | awk -F'[][]' '/%/ { print $2 }' | head -n1;
 }
 
+toggle_pavucontrol() {
+    if pgrep -x "pavucontrol-qt" > /dev/null; then
+        pkill -x "pavucontrol-qt"
+    else
+        i3-msg -q "exec --no-startup-id pavucontrol-qt"
+    fi
+}
+
 case $BLOCK_BUTTON in
     1) mute ;;  # Left click, mute
-    3) i3-msg -q "exec --no-startup-id pavucontrol-qt" ;;  # Right click, open pavucontrol
+    3) toggle_pavucontrol ;;  # Right click, open pavucontrol
     4) i3-volume up 5;;
     5) i3-volume down 5;;
 esac
