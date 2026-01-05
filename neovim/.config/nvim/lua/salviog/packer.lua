0f74d952a803b8bcafd8ea3135a144bf9c438484
Salvio Gargano <sungvzer@proton.me>
2026-01-05 13:02:51 +0100

    fix(nvim): vim.tbl_islist is deprecated

--- neovim/.config/nvim/lua/salviog/packer.lua
+++ neovim/.config/nvim/lua/salviog/packer.lua
@@ -28,8 +28,7 @@ require("packer").startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.3",
-		-- or                            , branch = '0.1.x',
+		branch = "0.1.x",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
