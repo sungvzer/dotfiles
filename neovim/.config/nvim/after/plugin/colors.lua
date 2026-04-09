1ffa722b33a55104e12b81ce2c05b34fdb59ab6c
sungvzer <sungvzer@proton.me>
2026-04-09 11:49:19 +0200

    feat(kitty): wallpaper and themes

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -1,18 +1,6 @@
 function ColorUpdate(color)
-	local new_color = color or "github_dark"
+	local new_color = color or "kanagawa"
 	vim.cmd.colorscheme(new_color)
-	-- If Terminal is KITTY, or inside TMUX with KITTY, set background to transparent
-	if os.getenv("TERM") == "xterm-kitty" or os.getenv("KITTY_WINDOW_ID") ~= nil then
-		vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
-		vim.cmd([[hi NormalNC guibg=NONE ctermbg=NONE]])
-		vim.cmd([[hi SignColumn guibg=NONE ctermbg=NONE]])
-
-		-- Telescope popup transparency
-		vim.cmd([[hi TelescopeNormal guibg=NONE ctermbg=NONE]])
-
-		-- NvimTree transparency
-		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
-	end
 end
 
 ColorUpdate()
