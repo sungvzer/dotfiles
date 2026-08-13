af9a37a14702dae6293fdc4e67d9a3bb42e9621c
Salvio Gargano <sungvzer@proton.me>
2026-08-13 14:34:08 +0200

    feat(i3): use eslint_d as linter

--- neovim/.config/nvim/lua/plugins/spec.lua
+++ neovim/.config/nvim/lua/plugins/spec.lua
@@ -4,7 +4,29 @@ return {
 	{
 		"folke/twilight.nvim",
 	},
-	{ "mfussenegger/nvim-lint" },
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+
+			-- 1. Assign eslint_d to your web development filetypes
+			lint.linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			}
+
+			-- 2. Create an autocommand to trigger linting
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 	{
 		"github/copilot.vim",
 	},
